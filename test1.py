5c52c4501b5a965da2b4de9e380ad6cf07d409b6
AhmedEssa12 <ae59250@gmail.com>
2025-05-15 12:47:32 +0300

    final

--- test1.py
+++ test1.py
@@ -3,15 +3,15 @@ from first_task import DFA_divided
 
 class TestDFA(unittest.TestCase):
     def test_accepted_cases(self):
-        self.assertEqual(DFA_divided("1101"), "Accepted")     # 3 ones
-        self.assertEqual(DFA_divided("1001"), "Rejected")     # 3 ones
-        self.assertEqual(DFA_divided("110010010101"), "Accepted")  # 6 ones
+        self.assertEqual(DFA_divided("1101"), "Accepted")    
+        self.assertEqual(DFA_divided("1001"), "Rejected")    
+        self.assertEqual(DFA_divided("110010010101"), "Accepted")  
 
     def test_rejected_cases(self):
-        self.assertEqual(DFA_divided("1011"), "Accepted")     # 4 ones
-        self.assertEqual(DFA_divided("0001"), "Rejected")     # 1 one
-        self.assertEqual(DFA_divided("11000000000000001111"), "Accepted")  # 7 ones
-        self.assertEqual(DFA_divided("1100"), "Rejected")     # 2 ones
+        self.assertEqual(DFA_divided("1011"), "Accepted")     
+        self.assertEqual(DFA_divided("0001"), "Rejected")     
+        self.assertEqual(DFA_divided("11000000000000001111"), "Accepted") 
+        self.assertEqual(DFA_divided("1100"), "Rejected")    
 
     def test_invalid_input(self):
         self.assertEqual(DFA_divided("12000"), "Invalid input")
